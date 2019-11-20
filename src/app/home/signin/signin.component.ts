087c1ae3f6b2c7f36e2c4682623c417b56acf666
Edson de Senna Junior <edson.junior@inatel.br>
2019-11-20 09:11:29 -0300

    Created Login Page and Validation

---
+++ src/app/home/signin/signin.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+    templateUrl: './signin.component.html'
+})
+export class SigninComponent {
+
+    loginForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+}
