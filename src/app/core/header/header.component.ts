398ea5fcbec2459241494d14c29abcc38efed59a
Edson de Senna Junior <edson.junior@inatel.br>
2019-11-22 08:41:51 -0300

    Auth Header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { UserService } from '../user/user.service';
+import { User } from '../user/user';
 
 @Component({
     selector: 'ap-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+    user$: Observable<User>;
+    
+    constructor(
+        private userService: UserService,
+        private router: Router
+    ) {
+        this.user$ = this.userService.getUser();
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
+    }
+}
