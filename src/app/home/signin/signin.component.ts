cc806bdc89c8727ba92806c710cd84f5edc72dc3
Edson de Senna Junior <edson.junior@inatel.br>
2019-11-22 08:12:01 -0300

    Token Service / Header Component

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { AuthService } from '../../core/auth.service';
+import { AuthService } from '../../core/auth/auth.service';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -10,11 +11,13 @@ import { AuthService } from '../../core/auth.service';
 export class SigninComponent {
 
     loginForm: FormGroup;
+    @ViewChild('userNameInput', {static: false}) userNameInput: ElementRef<HTMLInputElement>;
 
     constructor(
         private formBuilder: FormBuilder,
         private authService: AuthService,
-        private router: Router
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService
         ) {}
 
     ngOnInit() {
@@ -36,7 +39,12 @@ export class SigninComponent {
                 () => {
                     this.router.navigate(['user', userName])
                 },
-                () => this.loginForm.reset()
+                () =>{
+                    this.loginForm.reset()
+                    this.platformDetectorService.isPlatformBrowser() &&
+                        this.userNameInput.nativeElement.focus();
+                    alert('Invalid user name or password provided! Retry!');
+                } 
             );
     }
 }
