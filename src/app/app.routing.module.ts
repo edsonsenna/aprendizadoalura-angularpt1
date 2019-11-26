8928be7f3ef7fe14433d7e915bff656c4785734e
Edson de Senna Junior <edson.junior@inatel.br>
2019-11-26 09:36:01 -0300

    Lazy Loading Maximum Error

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,20 +5,16 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
-import { SigninComponent } from './home/signin/signin.component';
-import { AuthGuard } from './core/auth/auth.guard';
-import { SignupComponent } from './home/signup/signup.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
     { 
-        path: '' , 
-        component: HomeComponent,
-        canActivate: [AuthGuard],
-        children: [
-            { path: '' , component: SigninComponent},
-            { path: 'signup' , component: SignupComponent},
-        ]
+        path: 'home',
+        loadChildren:  () => import('./home/home.module').then(m => m.HomeModule)
     },
     { path: 'user/:userName' , component: PhotoListComponent, resolve: { photos: PhotoListResolver }},
     { path: 'p/add' , component: PhotoFormComponent},
