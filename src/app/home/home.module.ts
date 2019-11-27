ea41e07289db0aa60ade11789ca69f2a59b918e6
Edson de Senna Junior <edson.junior@inatel.br>
2019-11-27 09:33:30 -0300

    Http Interceptor

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,19 +3,25 @@ import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { RouterModule } from '@angular/router';
 
-import { SigninComponent } from "./signin/signin.component";
-import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
-import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home.routing.module';
+import { SigninComponent } from "./signin/signin.component";
+import { SignupComponent } from './signup/signup.component';
+import { SignupService } from './signup/signup.service';
+import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
 
 @NgModule({
   declarations: [HomeComponent, SigninComponent, SignupComponent],
   imports: [
       CommonModule,
+      HomeRoutingModule,
       FormsModule,
       ReactiveFormsModule,
       RouterModule, 
       VMessageModule
-    ]
+  ],
+  providers: [
+    SignupService
+  ]
 })
 export class HomeModule {}
